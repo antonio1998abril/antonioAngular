5e2d9374327f8fd9c02f14c11c468f69bdb69579
Antonio Rodriguez <anrodriguez@apexsystems.com>
2024-04-19 05:45:46 -0600

    7. Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'antonioAngular';
+  customName?:string  = ' antonio rodriguez barrientos';
+  customNumber?:number= 2500;
+
 }
